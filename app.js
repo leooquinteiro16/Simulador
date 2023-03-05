82d326b311a481b2a1d13f25db6b1d47e287562e
Leonardo Quinteiro <leoquinteiro_97@hotmail.com>
2023-03-05 20:59:26 -0300

    modificacion segunda entrega

--- app.js
+++ app.js
@@ -8,49 +8,43 @@ while(contraseña !== '1234'){
 
 alert('Bienvenido ' + usuario)
 
-function calcular()  {
-            let operacionPedida = prompt('Bienvenido a su calculadora, digannos que operacion quiere realizar')
+function compra()  {
+            let compraUsuario = prompt('Bienvenido a su BullDog indumentaria , digannos que categoria estaba buscando')
 
-                if (operacionPedida == '*')
-                {
-                    alert('Eligio Multiplicacion')
-                    primerNumero = Number(prompt('Indique su numero'))
-                    segundoNumero = Number(prompt('Indique su numero'))
-                    producto = primerNumero * segundoNumero
-                    alert('El resultado es ' + producto)
+                if (compraUsuario == 'Camisetas'){
+                alert ('El valor de cada camiseta es de $3.500')
+                cantidadDeseada = prompt('¿Cuantas cantidades necesita?')
+                calculoCompraFinal = cantidadDeseada * 3500 
+                alert ('Su precio total es de ' + calculoCompraFinal)
 
-                } else if (operacionPedida == '/') {
-                    alert('Eligio Division')
-                    primerNumero = Number(prompt('Indique su numero'))
-                    segundoNumero = Number(prompt('Indique su numero'))
-                    producto = primerNumero / segundoNumero
-                    alert('El resultado es ' + producto)
-                
 
-                } else if (operacionPedida == '+'){
-                
-                    alert('Eligio Suma')
-                    primerNumero = Number(prompt('Indique su 1er numero'))
-                    segundoNumero = Number(prompt('Indique su 2do numero'))
-                    primerNumero + segundoNumero
-                    producto = primerNumero + segundoNumero
-                    alert('El resultado es ' + producto)
+                } else if (compraUsuario == 'Pantalones') {
+                alert ('El valor de cada pantalon es de $4.000')
+                cantidadDeseada = prompt('¿Cuantas cantidades necesita?')
+                calculoCompraFinal = cantidadDeseada * 4000 
+                alert ('Su precio total es de ' + calculoCompraFinal)
 
-                }  else if (operacionPedida == '-'){
-                    alert('Eligio Resta')
-                    primerNumero = Number(prompt('Indique su 1er numero'))
-                    segundoNumero = Number(prompt('Indique su 2do numero'))
-                    producto = primerNumero - segundoNumero
-                    alert('El resultado es ' + producto)
+                } else if (compraUsuario == 'Zapatillas'){
+                alert ('El valor de cada par de zapatillas es de $10.000')
+                cantidadDeseada = prompt('¿Cuantas cantidades necesita?')
+                calculoCompraFinal = cantidadDeseada * 10000 
+                alert ('Su precio total es de ' + calculoCompraFinal)
+
+                }  else if (compraUsuario == 'Camperas'){
+                alert ('El valor de cada campera es de $6000')
+                cantidadDeseada = prompt('¿Cuantas cantidades necesita?')
+                calculoCompraFinal = cantidadDeseada * 6000 
+                alert ('Su precio total es de ' + calculoCompraFinal)
                 }  
+
                 
-                otraOperacion = prompt('¿Desea realizar otra operacion?')
-                    if (otraOperacion == 'si'){
-                        calcular()
-                    }   else if (otraOperacion === 'no'){
-                        alert('Muchas gracias por usar nuestra calculadora')
+                otraCompra = prompt('¿Desea realizar otra compra?')
+                    if (otraCompra == 'si'){
+                        compra()
+                    }   else if (otraCompra === 'no'){
+                        alert('Muchas gracias por visitarnos!')
                     }
         }
+compra()        
 
     
-calcular()
